bf49da03fcc443ac2881a480d1078084af8d6236
Tsend-Ayush Bayarmaa <tsendayushh@gmail.com>
2025-09-29 12:20:35 +0900

    learning log model indexing

--- daily_aggregator/models.py
+++ daily_aggregator/models.py
@@ -28,3 +28,8 @@ class LearningLog(models.Model):
         verbose_name_plural = "Learning Logs"
 
         unique_together = ("user", "timestamp") # Ensuring idempotency
+
+        # by indexing user and timestamp for faster queries
+        indexes = [
+            models.Index(fields=['user', 'timestamp']),  # Primary query pattern
+        ]
